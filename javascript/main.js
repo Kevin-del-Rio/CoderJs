f8bb774ee602deae853cd724f7d7f450525b1dec
kevin.delRio <kevindelrio1993@gmail.com>
2022-08-29 20:28:02 -0300

    se agrega moneda en mercadolibre segun el producto, se agrega el btn atras para volver a ver los filtros y regresar a nuestra tienda.

--- javascript/main.js
+++ javascript/main.js
@@ -25,6 +25,7 @@ const checkboxMayor = document.getElementById('mayor')
 const filtros = document.querySelector('.filtros__category')
 const categorias = document.querySelector('.filtros__checkbox')
 const input = document.querySelector(".input-buscar")
+const btnAtras = document.querySelector(".atras")
 let carrito = []
 let productosFiltrados = stockProductos;
 
@@ -225,6 +226,14 @@ function sleep(milliseconds) {
     } while (currentDate - date < milliseconds);
 }
 // -------------FETCH---------------------
+function ver(){
+    btnAtras.classList.remove("atras-ver")
+    filtros.classList.remove("remove-f")
+    categorias.classList.remove("remove-f")
+    input.value = ""
+    mostrasProductos(productosFiltrados)
+}
+
 const API_URL = 'https://api.mercadolibre.com/';
 const endpointProductos = 'sites/MLU/search/';
 let productoss;
@@ -249,7 +258,7 @@ const buscarProductosEnMELI = (productoAbuscar) => {
         <p class="text__body">${prod.title}</p>
         </div>
         <div class="card__footer">
-        <span class="text__title">$${prod.price}</span>      
+        <span class="text__title">${prod.currency_id} ${prod.price}</span>      
       <div class="card__button"><a target="_blank" href="${prod.permalink}">
         <svg class="svg__icon" viewBox="0 0 20 20">
             <path d="M17.72,5.011H8.026c-0.271,0-0.49,0.219-0.49,0.489c0,0.271,0.219,0.489,0.49,0.489h8.962l-1.979,4.773H6.763L4.935,5.343C4.926,5.316,4.897,5.309,4.884,5.286c-0.011-0.024,0-0.051-0.017-0.074C4.833,5.166,4.025,4.081,2.33,3.908C2.068,3.883,1.822,4.075,1.795,4.344C1.767,4.612,1.962,4.853,2.231,4.88c1.143,0.118,1.703,0.738,1.808,0.866l1.91,5.661c0.066,0.199,0.252,0.333,0.463,0.333h8.924c0.116,0,0.22-0.053,0.308-0.128c0.027-0.023,0.042-0.048,0.063-0.076c0.026-0.034,0.063-0.058,0.08-0.099l2.384-5.75c0.062-0.151,0.046-0.323-0.045-0.458C18.036,5.092,17.883,5.011,17.72,5.011z"></path>
@@ -266,7 +275,7 @@ const buscarProductosEnMELI = (productoAbuscar) => {
             })
 
         })
-   
+    btnAtras.classList.add("atras-ver")
     filtros.classList.add("remove-f")
     categorias.classList.add("remove-f")
     
