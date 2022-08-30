15784979fa474e366f0701bd18b90cd556c68db3
kevin.delRio <kevindelrio1993@gmail.com>
2022-08-29 22:09:46 -0300

    entrega

--- javascript/main.js
+++ javascript/main.js
@@ -12,6 +12,7 @@ let stockProductos = [
     { id: 11, categoria: "Accesorio", nombre: "Lentes", img: "../img/catalogo/lentes.png", precio: 125, cantidad: 1 },
     { id: 12, categoria: "Vestimenta", nombre: "Remeras", img: "../img/catalogo/remera.png", precio: 1500, cantidad: 1 },
 ]
+
 const contenedorProductos = document.getElementById('producto')
 const carritoCenter = document.querySelector(".carrito__center")
 const carritoDOM = document.querySelector(".carrito")
@@ -148,7 +149,7 @@ function categoryValue() {
             icon: 'info',
             confirmButtonText: 'Seguir Comprando'
         })
-        document.getElementById("category").value = "Todos"    
+        document.getElementById("category").value = "Todos"
         productosFiltrados = stockProductos;
         mostrasProductos(productosFiltrados)
     }
@@ -189,7 +190,7 @@ function mostrasProductos(productosCat) {
         const idButton = `add-cart${prod.id}`
         const div = document.createElement('div')
         div.classList.add('card')
-        div.innerHTML = ` 
+        div.innerHTML = `
         <div class="card__img">
             <div class="max">
                 <img src="${prod.img}"alt="imagen producto">
@@ -226,7 +227,7 @@ function sleep(milliseconds) {
     } while (currentDate - date < milliseconds);
 }
 // -------------FETCH---------------------
-function ver(){
+function ver() {
     btnAtras.classList.remove("atras-ver")
     filtros.classList.remove("remove-f")
     categorias.classList.remove("remove-f")
@@ -237,7 +238,7 @@ function ver(){
 const API_URL = 'https://api.mercadolibre.com/';
 const endpointProductos = 'sites/MLU/search/';
 let productoss;
-const buscarProductosEnMELI = (productoAbuscar) => {  
+const buscarProductosEnMELI = (productoAbuscar) => {
     fetch(API_URL + endpointProductos + '?q=' + productoAbuscar)
         .then((response) => response.json())
         .then((data) => {
@@ -247,7 +248,7 @@ const buscarProductosEnMELI = (productoAbuscar) => {
             productoss.forEach((prod) => {
                 const div = document.createElement('div')
                 div.classList.add('card')
-                div.innerHTML = ` 
+                div.innerHTML = `
         <div class="card__img">
             <div class="max">
                 <img src="${prod.thumbnail}"alt="imagen producto">
@@ -258,7 +259,7 @@ const buscarProductosEnMELI = (productoAbuscar) => {
         <p class="text__body">${prod.title}</p>
         </div>
         <div class="card__footer">
-        <span class="text__title">${prod.currency_id} ${prod.price}</span>      
+        <span class="text__title">${prod.currency_id} ${prod.price}</span>
       <div class="card__button"><a target="_blank" href="${prod.permalink}">
         <svg class="svg__icon" viewBox="0 0 20 20">
             <path d="M17.72,5.011H8.026c-0.271,0-0.49,0.219-0.49,0.489c0,0.271,0.219,0.489,0.49,0.489h8.962l-1.979,4.773H6.763L4.935,5.343C4.926,5.316,4.897,5.309,4.884,5.286c-0.011-0.024,0-0.051-0.017-0.074C4.833,5.166,4.025,4.081,2.33,3.908C2.068,3.883,1.822,4.075,1.795,4.344C1.767,4.612,1.962,4.853,2.231,4.88c1.143,0.118,1.703,0.738,1.808,0.866l1.91,5.661c0.066,0.199,0.252,0.333,0.463,0.333h8.924c0.116,0,0.22-0.053,0.308-0.128c0.027-0.023,0.042-0.048,0.063-0.076c0.026-0.034,0.063-0.058,0.08-0.099l2.384-5.75c0.062-0.151,0.046-0.323-0.045-0.458C18.036,5.092,17.883,5.011,17.72,5.011z"></path>
@@ -278,7 +279,7 @@ const buscarProductosEnMELI = (productoAbuscar) => {
     btnAtras.classList.add("atras-ver")
     filtros.classList.add("remove-f")
     categorias.classList.add("remove-f")
-    
+
 }
 
 
